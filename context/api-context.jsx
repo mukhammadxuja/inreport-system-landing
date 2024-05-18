b61f6a6434f643c32d913b097995cf60819efe61
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-05-19 01:57:14 +0500

    performance: get profile data

--- context/api-context.jsx
+++ context/api-context.jsx
@@ -72,93 +72,145 @@ export const ApiContextProvider = ({ children }) => {
     }
   };
 
-  // Profile
-  const awardsCollection = collection(db, `users/${userUid}/awards`);
-  const contactsCollection = collection(db, `users/${userUid}/contacts`);
-  const projectsCollection = collection(db, `users/${userUid}/projects`);
-  const sideProjectsCollection = collection(
-    db,
-    `users/${userUid}/side-projects`
-  );
-  const experiencesCollection = collection(db, `users/${userUid}/experiences`);
-  const educationsCollection = collection(db, `users/${userUid}/educations`);
-  const volunteeringsCollection = collection(
-    db,
-    `users/${userUid}/volunteerings`
-  );
-  const certificationsCollection = collection(
-    db,
-    `users/${userUid}/certifications`
-  );
-
-  // messages
-  const messagesCollection = collection(db, `users/${userUid}/messages`);
+  useEffect(() => {
+    if (userUid) {
+      const awardsCollection = collection(db, `users/${userUid}/awards`);
+      const queryAwards = query(awardsCollection);
+      const unsubscribeAwards = onSnapshot(queryAwards, (snapshot) => {
+        setAwards(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      });
+      return () => unsubscribeAwards();
+    }
+  }, [userUid]);
 
   useEffect(() => {
-    // fetch awards
-    const queryAwards = query(awardsCollection);
-    onSnapshot(queryAwards, (snapshot) => {
-      setAwards(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    });
+    if (userUid) {
+      const contactsCollection = collection(db, `users/${userUid}/contacts`);
+      const queryContacts = query(contactsCollection);
+      const unsubscribeContacts = onSnapshot(queryContacts, (snapshot) => {
+        setContacts(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        );
+      });
+      return () => unsubscribeContacts();
+    }
+  }, [userUid]);
 
-    // fetch contacts
-    const queryContacts = query(contactsCollection);
-    onSnapshot(queryContacts, (snapshot) => {
-      setContacts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    });
+  useEffect(() => {
+    if (userUid) {
+      const projectsCollection = collection(db, `users/${userUid}/projects`);
+      const queryProjects = query(projectsCollection);
+      const unsubscribeProjects = onSnapshot(queryProjects, (snapshot) => {
+        setProjects(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        );
+      });
+      return () => unsubscribeProjects();
+    }
+  }, [userUid]);
 
-    // fetch projects
-    const queryProjects = query(projectsCollection);
-    onSnapshot(queryProjects, (snapshot) => {
-      setProjects(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    });
-
-    // fetch side projects
-    const querySideProjects = query(sideProjectsCollection);
-    onSnapshot(querySideProjects, (snapshot) => {
-      setSideProjects(
-        snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+  useEffect(() => {
+    if (userUid) {
+      const sideProjectsCollection = collection(
+        db,
+        `users/${userUid}/sideProjects`
       );
-    });
-
-    // fetch side experience
-    const queryExperience = query(experiencesCollection);
-    onSnapshot(queryExperience, (snapshot) => {
-      setExperiences(
-        snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      const querySideProjects = query(sideProjectsCollection);
+      const unsubscribeSideProjects = onSnapshot(
+        querySideProjects,
+        (snapshot) => {
+          setSideProjects(
+            snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+          );
+        }
       );
-    });
+      return () => unsubscribeSideProjects();
+    }
+  }, [userUid]);
 
-    // fetch side education
-    const queryEducation = query(educationsCollection);
-    onSnapshot(queryEducation, (snapshot) => {
-      setEducations(
-        snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+  useEffect(() => {
+    if (userUid) {
+      const experiencesCollection = collection(
+        db,
+        `users/${userUid}/experiences`
       );
-    });
+      const queryExperience = query(experiencesCollection);
+      const unsubscribeExperience = onSnapshot(queryExperience, (snapshot) => {
+        setExperiences(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        );
+      });
+      return () => unsubscribeExperience();
+    }
+  }, [userUid]);
 
-    // fetch side volunteerings
-    const queryVolunteering = query(volunteeringsCollection);
-    onSnapshot(queryVolunteering, (snapshot) => {
-      setVolunteerings(
-        snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+  useEffect(() => {
+    if (userUid) {
+      const educationsCollection = collection(
+        db,
+        `users/${userUid}/educations`
       );
-    });
+      const queryEducation = query(educationsCollection);
+      const unsubscribeEducation = onSnapshot(queryEducation, (snapshot) => {
+        setEducations(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        );
+      });
+      return () => unsubscribeEducation();
+    }
+  }, [userUid]);
 
-    // fetch side volunteerings
-    const queryCertification = query(certificationsCollection);
-    onSnapshot(queryCertification, (snapshot) => {
-      setCertifications(
-        snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+  useEffect(() => {
+    if (userUid) {
+      const volunteeringsCollection = collection(
+        db,
+        `users/${userUid}/volunteerings`
       );
-    });
+      const queryVolunteering = query(volunteeringsCollection);
+      const unsubscribeVolunteering = onSnapshot(
+        queryVolunteering,
+        (snapshot) => {
+          setVolunteerings(
+            snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+          );
+        }
+      );
+      return () => unsubscribeVolunteering();
+    }
+  }, [userUid]);
 
-    // fetch messages
-    const queryMessages = query(messagesCollection);
-    onSnapshot(queryMessages, (snapshot) => {
-      setMessages(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    });
-  }, [auth.currentUser]);
+  useEffect(() => {
+    if (userUid) {
+      const certificationsCollection = collection(
+        db,
+        `users/${userUid}/certifications`
+      );
+      const queryCertification = query(certificationsCollection);
+      const unsubscribeCertification = onSnapshot(
+        queryCertification,
+        (snapshot) => {
+          setCertifications(
+            snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+          );
+        }
+      );
+      return () => unsubscribeCertification();
+    }
+  }, [userUid]);
+
+  useEffect(() => {
+    if (userUid) {
+      const messagesCollection = collection(db, `users/${userUid}/messages`);
+      const queryMessages = query(messagesCollection);
+      const unsubscribeMessages = onSnapshot(queryMessages, (snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        );
+      });
+      return () => unsubscribeMessages();
+    }
+  }, [userUid]);
+
 
   // Get user from google auth
   useEffect(() => {
