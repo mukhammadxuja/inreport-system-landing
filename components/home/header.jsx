aa051d893624d41d3568ff128a8485724454fa30
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-07-18 18:30:40 +0500

    add header grid

--- components/home/header.jsx
+++ components/home/header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
@@ -40,7 +41,7 @@ function Header() {
     <header className="container m-auto">
       <h1
         ref={textRef}
-        className="inline-block mt-24 mb-10 md:mt-24 lg:mb-28 lg:mt-36 tracking-[1.12] leading-[-.005em] text-clamp font-bold"
+        className="inline-block mt-24 mb-10 md:mt-24 lg:mb-24 lg:mt-36 tracking-[1.12] leading-[-.005em] text-clamp font-bold"
       >
         <span style={{ display: "inline-block" }}>Tovarlar.</span>
         <span style={{ display: "inline-block" }}>Kassa.</span> <br />
@@ -55,9 +56,95 @@ function Header() {
         </span>
       </h1>
 
-      <section id="about">
+      <section ref={image} id="about">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-4 pb-[48px]">
+          <a
+            href="/projects/mishmash"
+            className="flex gap-4 rounded-xl p-2 hover:bg-accent"
+          >
+            <img
+              className="h-18 w-24 flex-shrink-0 rounded-md bg-foreground-tertiary/10 object-cover object-center"
+              src="https://a.storyblok.com/f/198185/1600x1200/d6164a5867/mishmash3.jpg/m/200x160/"
+              alt=""
+              width="1600"
+              height="1200"
+            />
+            <div className="flex flex-col flex-1 space-y-1">
+              <p className="line-clamp-2 max-w-sm text-lg/tight font-medium">
+                mishmash. Unique e-commerce design for the ultimate product
+                experience.
+              </p>
+              <p className="text-sm text-foreground-secondary">
+                From our projects
+              </p>
+            </div>
+          </a>
+          <a
+            href="/projects/mishmash"
+            className="flex gap-4 rounded-xl p-2 hover:bg-accent"
+          >
+            <img
+              className="h-18 w-24 flex-shrink-0 rounded-md bg-foreground-tertiary/10 object-cover object-center"
+              src="https://a.storyblok.com/f/198185/1600x1200/d6164a5867/mishmash3.jpg/m/200x160/"
+              alt=""
+              width="1600"
+              height="1200"
+            />
+            <div className="flex flex-col flex-1 space-y-1">
+              <p className="line-clamp-2 max-w-sm text-lg/tight font-medium">
+                mishmash. Unique e-commerce design for the ultimate product
+                experience.
+              </p>
+              <p className="text-sm text-foreground-secondary">
+                From our projects
+              </p>
+            </div>
+          </a>
+          <a
+            href="/projects/mishmash"
+            className="flex gap-4 rounded-xl p-2 hover:bg-accent"
+          >
+            <img
+              className="h-18 w-24 flex-shrink-0 rounded-md bg-foreground-tertiary/10 object-cover object-center"
+              src="https://a.storyblok.com/f/198185/1600x1200/d6164a5867/mishmash3.jpg/m/200x160/"
+              alt=""
+              width="1600"
+              height="1200"
+            />
+            <div className="flex flex-col flex-1 space-y-1">
+              <p className="line-clamp-2 max-w-sm text-lg/tight font-medium">
+                mishmash. Unique e-commerce design for the ultimate product
+                experience.
+              </p>
+              <p className="text-sm text-foreground-secondary">
+                From our projects
+              </p>
+            </div>
+          </a>
+          <a
+            href="/projects/mishmash"
+            className="flex gap-4 rounded-xl p-2 hover:bg-accent"
+          >
+            <img
+              className="h-18 w-24 flex-shrink-0 rounded-md bg-foreground-tertiary/10 object-cover object-center"
+              src="https://a.storyblok.com/f/198185/1600x1200/d6164a5867/mishmash3.jpg/m/200x160/"
+              alt=""
+              width="1600"
+              height="1200"
+            />
+            <div className="flex flex-col flex-1 space-y-1">
+              <p className="line-clamp-2 max-w-sm text-lg/tight font-medium">
+                mishmash. Unique e-commerce design for the ultimate product
+                experience.
+              </p>
+              <p className="text-sm text-foreground-secondary">
+                From our projects
+              </p>
+            </div>
+          </a>
+        </div>
+
         <Image
-          ref={image}
           width={200}
           height={200}
           className="h-[60vh] lg:h-screen w-full rounded-lg object-cover bg-left"
