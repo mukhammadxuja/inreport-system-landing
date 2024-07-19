6eb5590102170fc7ddb12d75b2e3fefa675d2ff5
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-07-19 23:46:21 +0500

    types

--- components/home/types.jsx
+++ components/home/types.jsx
@@ -1,9 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import React, { useState } from "react";
-import Template from "./blog/template";
-import TemplateModal from "./blog/modal";
-import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
+import { Button } from "../ui/button";
+import { Send, Shirt, Store } from "lucide-react";
 
 const projects = [
   {
@@ -46,14 +45,115 @@ const projects = [
 
 function Types() {
   const [modal, setModal] = useState({ active: false, index: 0 });
+  const [hoveredButton, setHoveredButton] = useState("kiyim");
+
+  const images = {
+    kiyim:
+      "https://billz.io/_next/image?url=https%3A%2F%2Fbillzwp.billz.work%2Fwp-content%2Fuploads%2F2022%2F06%2F10.webp&w=1920&q=75",
+    konselareya:
+      "https://billz.io/_next/image?url=https%3A%2F%2Fbillzwp.billz.work%2Fwp-content%2Fuploads%2F2022%2F06%2F12.webp&w=1920&q=75",
+  };
+
   return (
-    <section id="Types">
-      <div className="container mx-auto w-full mt-10 mb-5">
+    <section id="Types" className="container mx-auto my-10 md:my-16">
+      <div className="w-full mb-5">
         <h2 className="text-clamp-title items-center">
-          <span className="text-muted-foreground">Inreport.</span>
+          <span className="text-muted-foreground">Har qanday</span>
           <br />
-          har qanday do&apos;konga <br /> mos keladi.
+          do&apos;konga mos keladi.
         </h2>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 items-center gap-4 md:gap-8 lg:gap-12">
+        <div className="lg:col-span-2 py-5 space-y-4">
+          <div className="flex items-center gap-4">
+            <Button
+              variant="secondary"
+              onMouseEnter={() => setHoveredButton("kiyim")}
+              onMouseLeave={() => setHoveredButton("kiyim")}
+              className="flex items-center gap-4 w-full hover:bg-gray-200/80"
+            >
+              <Shirt className="w-4 h-4" />
+              <span>Kiyim do&apos;koni</span>
+            </Button>
+            <Button
+              variant="secondary"
+              onMouseEnter={() => setHoveredButton("konselareya")}
+              onMouseLeave={() => setHoveredButton("konselareya")}
+              className="flex items-center gap-4 w-full hover:bg-gray-200/80"
+            >
+              <Store className="w-4 h-4" />
+              <span>Kanselyariya do&apos;koni</span>
+            </Button>
+          </div>
+          <div className="flex items-center gap-4">
+            <Button
+              variant="secondary"
+              onMouseEnter={() => setHoveredButton("kiyim")}
+              onMouseLeave={() => setHoveredButton("kiyim")}
+              className="flex items-center gap-4 w-full hover:bg-gray-200/80"
+            >
+              <Shirt className="w-4 h-4" />
+              <span>Kiyim do&apos;koni</span>
+            </Button>
+            <Button
+              variant="secondary"
+              onMouseEnter={() => setHoveredButton("konselareya")}
+              onMouseLeave={() => setHoveredButton("konselareya")}
+              className="flex items-center gap-4 w-full hover:bg-gray-200/80"
+            >
+              <Store className="w-4 h-4" />
+              <span>Kanselyariya do&apos;koni</span>
+            </Button>
+          </div>
+          <div className="flex items-center gap-4">
+            <Button
+              variant="secondary"
+              onMouseEnter={() => setHoveredButton("kiyim")}
+              onMouseLeave={() => setHoveredButton("kiyim")}
+              className="flex items-center gap-4 w-full hover:bg-gray-200/80"
+            >
+              <Shirt className="w-4 h-4" />
+              <span>Kiyim do&apos;koni</span>
+            </Button>
+            <Button
+              variant="secondary"
+              onMouseEnter={() => setHoveredButton("konselareya")}
+              onMouseLeave={() => setHoveredButton("konselareya")}
+              className="flex items-center gap-4 w-full hover:bg-gray-200/80"
+            >
+              <Store className="w-4 h-4" />
+              <span>Kanselyariya do&apos;koni</span>
+            </Button>
+          </div>
+          <div className="flex items-center gap-4">
+            <Button
+              variant="secondary"
+              onMouseEnter={() => setHoveredButton("kiyim")}
+              onMouseLeave={() => setHoveredButton("kiyim")}
+              className="flex items-center gap-4 w-full hover:bg-gray-200/80"
+            >
+              <Shirt className="w-4 h-4" />
+              <span>Kiyim do&apos;koni</span>
+            </Button>
+            <Button
+              variant="secondary"
+              onMouseEnter={() => setHoveredButton("konselareya")}
+              onMouseLeave={() => setHoveredButton("konselareya")}
+              className="flex items-center gap-4 w-full hover:bg-gray-200/80"
+            >
+              <Store className="w-4 h-4" />
+              <span>Kanselyariya do&apos;koni</span>
+            </Button>
+          </div>
+        </div>
+        <div className="w-full h-full hidden lg:block">
+          <img
+            src={hoveredButton ? images[hoveredButton] : images.default}
+            alt="Hovered"
+            className="object-cover w-full h-full rounded-lg"
+          />
+        </div>
       </div>
     </section>
   );
