477821a30c77490327326eb122acedbe7b945714
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-09-11 15:54:00 +0500

    internalization working

--- components/icons/index.js
+++
@@ -1,3 +0,0 @@
-export { default as EllipsesIcon } from "./Ellipses.jsx";
-export { default as CircleHelpIcon } from "./CircleHelp.jsx";
-export { default as LoadingIcon } from "./Loading.jsx";
--- utils/providers.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { MainContextProvider } from "@/context/main-context";
-
-function Providers({ children }) {
-  return <MainContextProvider>{children}</MainContextProvider>;
-}
-
-export default Providers;
